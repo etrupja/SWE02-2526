dbf1d3726d7d47f86c24640a5da73cf82d759fd8
enianxhixha <exhixha24@epoka.edu.al>
2026-04-07 14:29:57 +0200

    Update classwork.js

--- classwork.js
+++ classwork.js
@@ -1,13 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Classwork API</title>
-</head>
-<body>
+fetch("PASTE_YOUR_API_URL_HERE", {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "YOUR_API_KEY",
+    "X-RapidAPI-Host": "YOUR_API_HOST"
+  }
+})
+.then(response => response.json())
+.then(data => {
+  document.getElementById("output").innerHTML = `
+    <h2>📍 Location</h2>
+    <p>Latitude: ${data.coord.lat}</p>
+    <p>Longitude: ${data.coord.lon}</p>
 
-<h1>API Data</h1>
-<div id="output"></div>
+    <h2>🌤 Weather</h2>
+    <p>Main: ${data.weather[0].main}</p>
+    <p>Description: ${data.weather[0].description}</p>
 
-<script src="classwork.js"></script>
-</body>
-</html>
+    <h2>🌡 Temperature</h2>
+    <p>Current: ${data.main.temp}</p>
+    <p>Feels like: ${data.main.feels_like}</p>
+    <p>Humidity: ${data.main.humidity}%</p>
+  `;
+})
+.catch(err => console.error(err));
